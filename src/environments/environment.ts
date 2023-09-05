2a833f3ae51e2ec35e60deca5ddbe33a8707643f
Gabriel Chazarreta <gfchazarreta@gmail.com>
2023-09-05 20:50:38 -0300

    Eliminar carpeta environments

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    apiMoviesKey: 'aef66bef0b704c59db7d71b4f7785508',
-    apiMoviesToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZWY2NmJlZjBiNzA0YzU5ZGI3ZDcxYjRmNzc4NTUwOCIsInN1YiI6IjYyNDkwYTBlZThhM2UxMDA2MmMwYTdiOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l_JBfLjMncZ3TznVaSVmvPnigWR2nFmPrTUX56mtG7E',
-    firebaseUrl: 'https://login-app-c24fc-default-rtdb.firebaseio.com'
-};
